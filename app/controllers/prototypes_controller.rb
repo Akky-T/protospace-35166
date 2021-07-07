9193b40409e612a33500e83e3f7aec41beaf2bf8
Heidiki <82322691+Heidiki@users.noreply.github.com>
2021-07-07 21:18:04 +0900

    提出後の修正３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -59,6 +59,6 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index2
-    redirect_to new_view_registration_path unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
